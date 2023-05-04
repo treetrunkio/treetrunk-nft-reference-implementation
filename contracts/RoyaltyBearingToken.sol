be35e4bc61c17e8af303595c436cdb40d042f3cb
Siyuan Fan <sf316@duke.edu>
2023-05-04 13:35:47 -0400

    Fix compiler error caused by condition check on checkRegisterPayment

--- contracts/RoyaltyBearingToken.sol
+++ contracts/RoyaltyBearingToken.sol
@@ -464,7 +464,7 @@ contract RoyaltyBearingToken is ERC721Burnable, ERC721Pausable, ERC721URIStorage
         require(_chainId == block.chainid, 'Transfer on wrong Blockchain');
 
         //check register payment
-        require(paymentModule.checkRegisterPayment(_buyer, _tokenIds, _payment, _tokenType));
+        require(paymentModule.checkRegisterPayment(_tokenIds[0], _buyer, _tokenType) != 0);
 
         _requireExistsAndOwned(_tokenIds, _seller);
 
