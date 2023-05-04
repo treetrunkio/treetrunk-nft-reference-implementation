1b1a5f2be615fd9a54345d0aac4366e978cc090b
Siyuan Fan <sf316@duke.edu>
2023-05-04 13:14:11 -0400

    Remove incorrect tests added to PaymentModule and RoyaltyPaymentModule tests

--- test/coverage/PaymentModule.test.js
+++ test/coverage/PaymentModule.test.js
@@ -15,13 +15,13 @@ contract('PaymentModule', (accounts) => {
 
     describe('Only owner can call update functions', async () => {
         it('addListNFT', async () => {
-            await truffleAssert.reverts(paymentModule.addListNFT(ZERO_ADDRESS, [0], 0, '', { from: accAdmin }), '');
+            await truffleAssert.reverts(paymentModule.addListNFT(ZERO_ADDRESS, [0], 0, '', { from: accAdmin }), 'Ownable: caller is not the owner');
         });
         it('addRegisterPayment', async () => {
-            await truffleAssert.reverts(paymentModule.addRegisterPayment(ZERO_ADDRESS, [0], 0, '', { from: accAdmin }), '');
+            await truffleAssert.reverts(paymentModule.addRegisterPayment(ZERO_ADDRESS, [0], 0, '', { from: accAdmin }), 'Ownable: caller is not the owner');
         });
         it('removeRegisterPayment', async () => {
-            await truffleAssert.reverts(paymentModule.removeRegisterPayment(ZERO_ADDRESS, [0], { from: accAdmin }), '');
+            await truffleAssert.reverts(paymentModule.removeRegisterPayment(ZERO_ADDRESS, [0], { from: accAdmin }), 'Ownable: caller is not the owner');
         });
     });
 
@@ -35,47 +35,5 @@ contract('PaymentModule', (accounts) => {
             assert.equal(payment, 0);
         });
     });
-
-    describe('Exist in NFT List', async =>{
-        it('The seller of current token is the input address', async =>{
-            assert,equal(ZERO_ADDRESS, accUser1);
-        });
-    });
-
-    describe('addListNFT',async () =>{
-        it('Check NFT Price', async () =>{
-            const result = await paymentModule.addListNFT(ZERO_ADDRESS, [0], 0, '', { from: accAdmin });
-            assert.equal(result.price,0);
-        });
-        it('Check if is already exist', async () =>{
-            const result = await paymentModule.addListNFT(ZERO_ADDRESS, [0], 0, '', { from: accAdmin });
-            assert(existsInListNFT(result.tokenId));
-        });
-        it('Check number of tokens listed', async() => {
-            const result = await paymentModule.addListNFT(ZERO_ADDRESS, [0], 0, '', { from: accAdmin });
-            const tokenIds = result.tokenIds;
-            assert(tokenIds.length <= _maxListingNumber);
-        });
-    });
-
-    describe('removeListNFT', async =>{
-        it('Address of buyer should match', async() =>{
-            const result = await paymentModule.removeRegisterPayment(ZERO_ADDRESS, [0], { from: accAdmin }, '');
-            const buyer_address = registeredPayment[tokenId].buyer;
-            assert.equal(buyer_address,result.buyer);
-        })
-    });
-    describe('checkRegisteredPayment', async() =>{
-        it('buyer of the token should match', async() =>{
-            const result = await paymentModule.RegistedPayment(ZERO_ADDRESS, 0,0,'', { from: accAdmin });
-            const check_buyer = await paymentModule.checkRegistedPayment(ZERO_ADDRESS, 0,0,'', { from: accAdmin });
-            assert.equal(result[tokenId].buyer, check_buyer.buyer);
-        });
-        it('Peyment should equal the registered volume', async() => {
-            const result = await paymentModule.RegistedPayment(ZERO_ADDRESS, 0,0,'', { from: accAdmin });
-            const check = await paymentModule.checkRegistedPayment(ZERO_ADDRESS, 0,0,'', { from: accAdmin });
-            assert.equal(result[tokenId].payment, check._payment);
-        })
-    })
 
 });
